dc6844d16e4620bd2a5ae0758c0a1626d7981e1e
Carlomos7 <cs525@njit.edu>
2025-10-01 10:54:50 -0400

    First commit

---
+++ src/main/java/com/carlomos/agents/conversational_agents/ConversationalAgentsApplication.java
@@ -0,0 +1,13 @@
+package com.carlomos.agents.conversational_agents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationalAgentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversationalAgentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlomos/agents/conversational_agents/ConversationalAgentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlomos.agents.conversational_agents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationalAgentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
