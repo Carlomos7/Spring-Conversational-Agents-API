30e4f2bfee4eed4ad57c0e2651846a21e1ad303a
Carlomos7 <cs525@njit.edu>
2025-10-01 13:03:34 -0400

    Feat: initial test for db using testcontainers

--- src/test/java/com/carlomos/agents/conversational_agents/ConversationalAgentsApplicationTests.java
+++ src/test/java/com/carlomos/agents/conversational_agents/ConversationalAgentsApplicationTests.java
@@ -1,9 +1,12 @@
 package com.carlomos.agents.conversational_agents;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = DatabaseTestConfig.Initializer.class)
 class ConversationalAgentsApplicationTests {
 
 	@Test
