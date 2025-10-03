1c4d69bfd6f4f1d94c92b001c6e52683ee5f38f6
Carlos <cs525@njit.edu>
2025-10-02 22:24:26 -0400

    Refac: adding copy method for update

--- src/main/java/com/carlomos/agents/mapper/AgentMapper.java
+++ src/main/java/com/carlomos/agents/mapper/AgentMapper.java
@@ -1,5 +1,7 @@
 package com.carlomos.agents.mapper;
 
+import java.util.List;
+
 import com.carlomos.agents.dto.request.agent.AgentRequest;
 import com.carlomos.agents.dto.response.agent.AgentResponse;
 import com.carlomos.agents.entity.Agent;
@@ -16,7 +18,16 @@ public class AgentMapper {
         );
     }
 
-    public static AgentResponse toDTO(Agent agent) {
+    // for PUT/PATCH operations
+    public static void copy(AgentRequest dto, Agent target){
+        target.setName(dto.name());
+        target.setContext(dto.context());
+        target.setFirstMessage(dto.firstMessage());
+        target.setResponseShape(dto.responseShape());
+        target.setInstructions(dto.instructions());
+    }
+
+    public static AgentResponse toResponse(Agent agent) {
         return new AgentResponse(
             agent.getId(),
             agent.getName(),
@@ -25,5 +36,9 @@ public class AgentMapper {
             agent.getResponseShape(),
             agent.getInstructions()
         );
+    }
+
+    public static List<AgentResponse> toResponseList(List<Agent> agents) {
+        return agents.stream().map(AgentMapper::toResponse).toList();
     }
 }
